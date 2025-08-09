ac1a663679e8ee31f8b33c28b4a205ace87c1ceb
szks-repo <szkscookie@gmail.com>
2025-08-10 00:37:03 +0900

    add(string): number_format

---
+++ string/number_format.go
@@ -0,0 +1,14 @@
+package string
+
+import (
+	"golang.org/x/exp/constraints"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"golang.org/x/text/number"
+)
+
+var msgPrinter = message.NewPrinter(language.Japanese)
+
+func NumberFormat[N constraints.Integer | constraints.Float](n N) string {
+	return msgPrinter.Sprint(number.Decimal(n))
+}
