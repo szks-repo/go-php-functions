73fb13d240925b28ad30f2d7c4f6ac4e65368f6b
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-09-30 20:01:56 +0900

    add: date_test case

--- string/date_test.go
+++ string/date_test.go
@@ -8,6 +8,8 @@ import (
 
 func TestDate(t *testing.T) {
 	fixed := time.Date(2025, time.September, 28, 15, 4, 5, 123456000, time.FixedZone("JST", 9*3600))
+	leap := time.Date(2024, time.February, 29, 6, 30, 45, 0, time.FixedZone("JST", 9*3600))
+	isoBoundary := time.Date(2022, time.January, 1, 8, 0, 0, 0, time.UTC)
 
 	originalNow := timeNow
 	timeNow = func() time.Time { return fixed }
@@ -17,6 +19,7 @@ func TestDate(t *testing.T) {
 		name   string
 		layout string
 		want   string
+		now    *time.Time
 	}{
 		{
 			name:   "basic",
@@ -44,14 +47,39 @@ func TestDate(t *testing.T) {
 			want:   "03:04:05 PM",
 		},
 		{
+			name:   "hour variants",
+			layout: "H-G-g-h a A",
+			want:   "15-15-3-03 pm PM",
+		},
+		{
 			name:   "ordinal and month",
 			layout: "jS F Y",
 			want:   "28th September 2025",
 		},
 		{
+			name:   "month variants",
+			layout: "y/n M D l N w",
+			want:   "25/9 Sep Sun Sunday 7 0",
+		},
+		{
+			name:   "day of year and iso week",
+			layout: "z o W",
+			want:   "270 2025 39",
+		},
+		{
 			name:   "timezone",
-			layout: "O P Z e",
-			want:   "+0900 +09:00 32400 JST",
+			layout: "T O P Z e",
+			want:   "JST +0900 +09:00 32400 JST",
+		},
+		{
+			name:   "microseconds and milliseconds",
+			layout: "u v",
+			want:   "123456 123",
+		},
+		{
+			name:   "days in month and leap",
+			layout: "t L",
+			want:   "30 0",
 		},
 		{
 			name:   "unix timestamp",
@@ -63,12 +91,37 @@ func TestDate(t *testing.T) {
 			layout: "",
 			want:   "",
 		},
+		{
+			name:   "leap year days",
+			layout: "t L",
+			want:   "29 1",
+			now:    &leap,
+		},
+		{
+			name:   "iso week boundary",
+			layout: "o-W-N",
+			want:   "2021-52-6",
+			now:    &isoBoundary,
+		},
 	}
 
-	for _, tt := range tests {
-		got := Date(tt.layout)
-		if got != tt.want {
-			t.Errorf("%s: expected %q, got %q", tt.name, tt.want, got)
-		}
+	for _, tc := range tests {
+		test := tc
+		t.Run(test.name, func(t *testing.T) {
+			target := fixed
+			if test.now != nil {
+				target = *test.now
+			}
+
+			timeNow = func() time.Time { return target }
+			t.Cleanup(func() {
+				timeNow = func() time.Time { return fixed }
+			})
+
+			got := Date(test.layout)
+			if got != test.want {
+				t.Errorf("layout %q: expected %q, got %q", test.layout, test.want, got)
+			}
+		})
 	}
 }
